0d76705ce3b9da6379d027d6d7595ba96eb161fa
littlej8 <jacksontravis2008@gmail.com>
2024-10-12 22:09:19 -0500

    add camera code that should outline samples

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CameraOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CameraOpMode.java
@@ -2,16 +2,17 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Path;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.function.Consumer;
 import org.firstinspires.ftc.robotcore.external.function.Continuation;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -20,13 +21,15 @@ import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
+import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvWebcam;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -34,10 +37,73 @@ import java.util.concurrent.atomic.AtomicReference;
 @Autonomous
 public class CameraOpMode extends LinearOpMode {
     public static class CameraStreamProcessor implements VisionProcessor, CameraStreamSource {
+        public static class Blob {
+            private MatOfPoint contour;
+            private Point[] contourPts;
+            private int area = -1;
+            private RotatedRect rect;
+
+            Blob(MatOfPoint contour) {
+                this.contour = contour;
+            }
+
+            public MatOfPoint getContour() {
+                return this.contour;
+            }
+
+            public Point[] getContourPts() {
+                if (this.contourPts == null) {
+                    this.contourPts = this.contour.toArray();
+                }
+
+                return this.contourPts;
+            }
+
+            public int getContourArea() {
+                if (this.area < 0) {
+                    this.area = Math.max(1, (int)Imgproc.contourArea(this.contour));
+                }
+
+                return this.area;
+            }
+
+            public RotatedRect getBoxFit() {
+                if (this.rect == null) {
+                    this.rect = Imgproc.minAreaRect(new MatOfPoint2f(this.getContourPts()));
+                }
+
+                return this.rect;
+            }
+
+            public static void filterByArea(List<Blob> blobsToFilter, double minArea, double maxArea) {
+                ArrayList<Blob> toRemove = new ArrayList<>();
+                Iterator<Blob> it = blobsToFilter.iterator();
+
+                while (true) {
+                    Blob b;
+
+                    do {
+                        if (!it.hasNext()) {
+                            blobsToFilter.removeAll(toRemove);
+                            return;
+                        }
+
+                        b = it.next();
+                    } while (!((double)b.getContourArea() > maxArea) && !((double)b.getContourArea() < minArea));
+
+                    toRemove.add(b);
+                }
+            }
+        }
+
         private final AtomicReference<Bitmap> lastFrame =
                 new AtomicReference<>(Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565));
 
-        List<MatOfPoint> contoursList = new ArrayList<>();
+        public static Scalar[] BLUE_THRESH = {new Scalar(16.0, 0.0, 155.0), new Scalar(255.0, 127.0, 255.0)};
+        public static Scalar[] RED_THRESH = {new Scalar(32.0, 176.0, 0.0), new Scalar(255.0, 255.0, 132.0)};
+        public static Scalar[] YELLOW_THRESH = {new Scalar(32.0, 128.0, 0.0), new Scalar(255.0, 170.0, 120.0)};
+
+        List<Blob> blobs = new ArrayList<>();
 
         @Override
         public void init(int width, int height, CameraCalibration calibration) {
@@ -46,46 +112,38 @@ public class CameraOpMode extends LinearOpMode {
 
         @Override
         public Object processFrame(Mat input, long captureTimeNanos) {
-            contoursList.clear();
+            blobs.clear();
 
-            Mat frame = new Mat();
-            input.copyTo(frame);
-            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGB2YCrCb);
-            Core.extractChannel(frame, frame, 2);
-            Imgproc.threshold(frame, frame, 102, 255, Imgproc.THRESH_BINARY_INV);
-            List<MatOfPoint> tmpContours = new ArrayList<>();
-            Imgproc.findContours(frame, tmpContours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+            Mat colorSpace = input.clone();
+            Mat mask = new Mat();
 
-            for (MatOfPoint contour : tmpContours) {
-                if (Imgproc.contourArea(contour) > 1000) {
-                    contoursList.add(contour);
+            Imgproc.cvtColor(input, colorSpace, Imgproc.COLOR_RGB2YCrCb);
+            Imgproc.GaussianBlur(colorSpace, colorSpace, new Size(5, 5), 0.0);
+
+            Core.inRange(colorSpace, YELLOW_THRESH[0], YELLOW_THRESH[1], mask);
+
+            List<MatOfPoint> contoursList = new ArrayList<>();
+            Mat heirarchy = new Mat();
+            Imgproc.findContours(mask, contoursList, heirarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            heirarchy.release();
+
+            for (MatOfPoint contour : contoursList) {
+                blobs.add(new Blob(contour));
+            }
+
+            Blob.filterByArea(blobs, 50, 20000);
+
+            for (Blob b : blobs) {
+                Point[] contourPts = b.getContourPts();
+                for (int i = 1; i < contourPts.length; i++) {
+                    Point last = contourPts[i-1];
+                    Point cur = contourPts[i];
+                    Imgproc.line(input, last, cur, new Scalar(3, 227, 252), 4);
                 }
             }
 
-            /*MatOfPoint largest = null;
-            double largestNum = 0;
-            List<Integer> toRemove = new ArrayList<>();
-            for (int i = 0; i < contoursList.size(); i++) {
-                MatOfPoint contour = contoursList.get(i);
-                double area = Imgproc.contourArea(contour);
-                if (area > largestNum) {
-                    largestNum = area;
-                    largest = contour;
-                }
-                if (area < 100) {
-                    contoursList.remove(i);
-                }
-            }
-
-            for (int remove : toRemove) {
-                if (remove >= 0 && remove < contoursList.size())
-                    contoursList.remove(remove);
-            }*/
-
-            Imgproc.drawContours(input, contoursList, -1, new Scalar(0, 0, 255), 3, 8);
-
-            Bitmap b = Bitmap.createBitmap(frame.width(), frame.height(), Bitmap.Config.RGB_565);
-            Utils.matToBitmap(frame, b);
+            Bitmap b = Bitmap.createBitmap(input.width(), input.height(), Bitmap.Config.RGB_565);
+            Utils.matToBitmap(input, b);
             lastFrame.set(b);
             return null;
         }
@@ -101,6 +159,19 @@ public class CameraOpMode extends LinearOpMode {
         public void getFrameBitmap(Continuation<? extends Consumer<Bitmap>> continuation) {
             continuation.dispatch(bitmapConsumer -> bitmapConsumer.accept(lastFrame.get()));
         }
+
+        public Blob getLargestBlob() {
+            if (blobs.isEmpty())
+                return null;
+
+            Blob largest = blobs.get(0);
+            for (Blob b : blobs) {
+                if (b.getContourArea() > largest.getContourArea())
+                    largest = b;
+            }
+
+            return largest;
+        }
     }
     @Override
     public void runOpMode() throws InterruptedException {
@@ -112,10 +183,16 @@ public class CameraOpMode extends LinearOpMode {
                 .build();
 
         FtcDashboard.getInstance().startCameraStream(processor, 0);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         waitForStart();
 
         while (opModeIsActive()) {
+            CameraStreamProcessor.Blob largestBlob = processor.getLargestBlob();
+
+            if (largestBlob != null)
+                telemetry.addData("Largest Blob Position", "(%d, %d)", largestBlob.getBoxFit().center.x, largestBlob.getBoxFit().center.y);
+
             sleep(100L);
         }
     }
