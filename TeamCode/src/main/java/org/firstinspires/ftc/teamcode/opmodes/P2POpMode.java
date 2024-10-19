b78f4016d0ba831f29c7904aa45c0eb0ac5552cb
littlej8 <jacksontravis2008@gmail.com>
2024-10-19 11:41:51 -0500

    rename opmode to fix file collisions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/P2POpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/P2POpMode.java
@@ -13,8 +13,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import java.util.ArrayList;
 import java.util.List;
 
-@TeleOp(name = "BareBonesP2POpMode")
-public class BareBonesP2POpMode extends LinearOpMode {
+@TeleOp(name = "P2POpMode")
+public class P2POpMode extends LinearOpMode {
     String[] motorNames = new String[]{"back_left_motor", "front_left_motor", "front_right_motor", "back_right_motor"};
     List<DcMotorEx> motors = new ArrayList<>();
 
