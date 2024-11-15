0439ddccc99364c6b97e61bc4a9b67a0b66d7cea
littlej8 <jacksontravis2008@gmail.com>
2024-11-15 11:49:24 -0600

    blocks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlocksFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlocksFunctions.java
@@ -5,12 +5,17 @@ import com.google.blocks.ftcrobotcontroller.runtime.Block;
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
 import org.firstinspires.ftc.robotcore.external.ExportClassToBlocks;
 import org.firstinspires.ftc.robotcore.external.ExportToBlocks;
+import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Slides;
 
 public class BlocksFunctions extends BlocksOpModeCompanion {
     public static Drivetrain dt;
+    public static Arm arm;
+    public static Slides slides;
+
     @ExportToBlocks(
-            comment = "Initializes the drivetrain",
+            heading = "",
             tooltip = "Initializes the drivetrain"
     )
     /*@Block(
@@ -22,7 +27,33 @@ public class BlocksFunctions extends BlocksOpModeCompanion {
     }
 
     @ExportToBlocks(
+            heading = "",
+            tooltip = "Initializes the arm"
+    )
+    public static void initArm() {
+        arm = new Arm(hardwareMap, telemetry);
+    }
 
+    @ExportToBlocks(
+            heading = "",
+            tooltip = "Initializes the slides"
+    )
+    public static void initSlides() {
+        slides = new Slides(hardwareMap, telemetry);
+    }
+
+    @ExportToBlocks(
+            heading = "",
+            tooltip = "Initializes the robot"
+    )
+    public static void initRobot() {
+        initDrivetrain();
+        initArm();
+        initSlides();
+    }
+
+    @ExportToBlocks(
+            heading = "",
             comment = "Moves the robot to the specified position in inches relative to its starting position",
             tooltip = "Moves the robot to a position",
             parameterLabels = {"X Position", "Y Position", "Heading"}
@@ -32,6 +63,7 @@ public class BlocksFunctions extends BlocksOpModeCompanion {
     }
 
     @ExportToBlocks(
+            heading = "",
             comment = "Holds the robot in the last position it was told to move to for the specified amount of milliseconds.",
             tooltip = "Maintains the robot's position for a length of time.",
             parameterLabels = "Time (Milliseconds)"
